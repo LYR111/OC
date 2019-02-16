e4ff06aa63336f7b8b85e86ec085783c047a3f40
刘亚茹 <1536828927@qq.com>
2019-02-16 22:57:59 +0800

    计算器2

---
+++ 加法计算器的副本/加法计算器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  加法计算器
+//
+//  Created by 刘亚茹 on 2019/2/16.
+//  Copyright © 2019 LIUYARU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
